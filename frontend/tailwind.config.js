cf0dc8bf13916143e5394fac35665a50cb02ec03
virajsaranga <virajridi@gmail.com>
2025-01-08 22:13:08 +0530

    add tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+//     darkMode: false, // or 'media' or 'class'
+//     theme: {
+//         extend: {},
+//     },
+//     variants: {
+//         extend: {},
+//     },
+//     plugins: [],    
+//}
+
+///** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
