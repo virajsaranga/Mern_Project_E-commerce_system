966103e4a71a528d6c8bc998b787b724c35403c6
virajsaranga <virajridi@gmail.com>
2025-01-21 10:10:13 +0530

    css updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,4 @@
-// module.exports = {
-//     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-//     darkMode: false, // or 'media' or 'class'
-//     theme: {
-//         extend: {},
-//     },
-//     variants: {
-//         extend: {},
-//     },
-//     plugins: [],    
-//}
 
-///** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
