179b726ffaec11972d2bcf32b214548aa96ec6c1
virajsaranga <virajridi@gmail.com>
2025-01-30 22:23:14 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Connect to MongoDB
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
