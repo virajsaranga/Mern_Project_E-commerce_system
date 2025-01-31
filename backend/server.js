57cac9909d58ddc3502318a06419c780d929b23b
virajsaranga <virajridi@gmail.com>
2025-01-31 22:39:49 +0530

    comment updated

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to the MERN E-Commerce Store API');
 });
 
-// Start the server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
